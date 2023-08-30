f5717302608ff88d9ee17168eaa56c90e20e8b0c
Akira <jinnseitanosi@gmail.com>
2023-08-30 18:16:46 +0900

    section10 done

--- section7_BigProjectTime/mycv/src/users/users.controller.ts
+++ section7_BigProjectTime/mycv/src/users/users.controller.ts
@@ -2,8 +2,11 @@ import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundExcep
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from '../interceptors/serialize.interceptors';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
@@ -12,6 +15,8 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    // @UseInterceptors(new SerializeInterceptor(UserDto))
+    // @Serialize(UserDto)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user = await this.usersService.findOne(parseInt(id));
