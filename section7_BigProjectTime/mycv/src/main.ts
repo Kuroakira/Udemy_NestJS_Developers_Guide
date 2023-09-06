886d9f5ad039e7a0ec05bec4ce5f0a8bbd649028
Akira <jinnseitanosi@gmail.com>
2023-09-06 18:48:35 +0900

    section13 done

--- section7_BigProjectTime/mycv/src/main.ts
+++ section7_BigProjectTime/mycv/src/main.ts
@@ -5,16 +5,6 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // keys will be used to encrypt values of cookie.
-  app.use(cookieSession({
-    keys: ['abcdefg']
-  }))
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
   await app.listen(3000);
 }
 bootstrap();
