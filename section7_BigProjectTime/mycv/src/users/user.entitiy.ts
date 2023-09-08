016250e26ef4618d4b02e0a9ac9a32898d2199d5
Akira <jinnseitanosi@gmail.com>
2023-09-08 16:49:55 +0900

    section15 done

--- section7_BigProjectTime/mycv/src/users/user.entitiy.ts
+++ section7_BigProjectTime/mycv/src/users/user.entitiy.ts
@@ -1,4 +1,10 @@
-import { AfterInsert, AfterRemove, AfterUpdate, Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { 
+    AfterInsert, 
+    AfterRemove, 
+    AfterUpdate,
+    Entity, 
+    Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Report } from '../reports/report.entity';
 
 @Entity()
 export class User {
@@ -10,6 +16,9 @@ export class User {
 
     @Column()
     password: string;
+
+    @OneToMany(() => Report, (report) => report.user)
+    reports: Report[];
 
     @AfterInsert()
     logInsert() {
